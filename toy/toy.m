16d7c1ff6021db87065d36b8b60b7dbb15074e53
Disha Shur <disha.shur@gmail.com>
2020-11-08 13:55:06 +0530

    Add files via upload

--- toy/toy.m
+++ toy/toy.m
@@ -10,3 +10,38 @@ toy_checkins = [1,randi([1,168]),toy_venue(randi([1,numel(toy_venue)])),randi([1
     3,randi([1,168]),toy_venue(randi([1,numel(toy_venue)])),randi([1,7]);...
     4,randi([1,168]),toy_venue(randi([1,numel(toy_venue)])),randi([1,7]);...
     5,randi([1,168]),toy_venue(randi([1,numel(toy_venue)])),randi([1,7])];
+
+%clipped data_set
+
+qty = 500;
+
+new_users_IDs = selected_users_IDs(1:qty);
+
+k = 1;
+for i=1:size(friendship_old,1)
+     if (friendship_old(i,1)<=qty)||(friendship_old(i,2)<=qty)
+            new_friendship_old(k,:)= friendship_old(i,:);
+            k=k+1;
+     end
+end
+
+%make sure all qty user ids are accounted for atleast
+%once in the friendship matrix
+found = ismember([1:qty],new_friendship_old);
+found = found';
+temp = find(found == 0); %ensure that all users are accounted for atleast once
+for i=1:numel(new_friendship_old)
+    if new_friendship_old(i)>qty
+        new_friendship_old(i)=randi([1,qty]);
+    end
+end
+
+
+
+k = 1;
+for i=1:size(selected_checkins,1)
+    if selected_checkins(i,1)<=qty
+        new_checkins(k,:) = selected_checkins(i,:);
+        k=k+1;
+    end
+end
